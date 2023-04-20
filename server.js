673126be33efdefb457d196098bbe957e516cb75
DanChous1 <azarov.daniil@inbox.ru>
2023-04-20 22:33:01 +0300

    some

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 const crypto = require('crypto')
+var pg = require("pg")
 
+//Сайт
 const app = express();
 app.use(express.static(__dirname));
 
 // const urlencodedParser = express.urlencoded({extended: false});
 var jParser = bodyParser.json();
 
+//Шифрование
 const cryptos = [ "aes-128-cbc", "blowfish", "cast", "des" ]  
 const iv8 = crypto.randomBytes(8);
-const iv15 = crypto.randomBytes(16);
+const iv16 = crypto.randomBytes(16);
+
+//БД
+var client = new pg.Client("postgres://postgres:1@localhost:5432/chipher");
+client.connect();
+client.query("INSERT INTO newtable values('woivgnds;fkjbdkjbnd;jbnd;kjfbndkfj;bnreaovd;k', '123')");
 
 app.post("/", jParser, function (req, res)
 {   
